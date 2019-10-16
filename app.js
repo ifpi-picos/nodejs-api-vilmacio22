b191aa3b9f029003f9d7847b3e9b60a089dba5a9
aluno <deyvidcarvalho7@gmail.com>
2019-10-16 14:46:19 -0300

    config

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/');
+const conectaMongoDB = require('./config/mongodb')
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/', routes);
+conectaMongoDB();
 
 app.listen(2999, () => console.log('App Online...'));
--- routes/postagem-routes.js
+++ routes/postagem-routes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const PostagensController = require('../controllers/postagens')
+const PostagemModel = require('../models/postagem')
 
-router.get('/', function(req, res){
-    res.send('lista de postagem');
-})
+const postagensController = new PostagensController(PostagemModel);
 
-router.post('/', function(req, res){
-    res.send('postagem salva')
-})
+router.get('/', async(req, res)=>{
+    //busca postagens no banco de dados
+    const postagens = await postagensController.consultarTodos();
+    res.send(postagens);
+});
+
+router.post('/', async(req, res) =>{
+    //salva postagem no banco de dados
+    const novaPostagem = req.body;
+    const retorno = await postagensController.adicionar(novaPostagem);
+    res.send(retorno);
+});
 
 module.exports = router;
 
