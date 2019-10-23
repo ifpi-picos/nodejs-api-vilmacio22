e48d254869362e6463d7f2fa2555f1b0b4212996
aluno <deyvidcarvalho7@gmail.com>
2019-10-23 11:58:25 -0300

    by id

--- controllers/postagens.js
+++ controllers/postagens.js
@@ -12,6 +12,18 @@ class Postagens{
         const postagem = await this.Postagem.find({});
         return postagem;
     }
+
+    async alterarPorId(id, postagemDT0){
+        await this.Postagem.updateOne({_id: id}, postagemDT0)
+    }
+
+    async consultarPorId(id){
+        const postagem = await this.Postagem.findById(id);
+        return postagem;
+    }
+    async deletePorId(id){
+        await this.Postagem.deleteOne({_id: id});
+    }
 }
 
 module.exports = Postagens;
